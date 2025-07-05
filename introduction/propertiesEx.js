47f94567e70acda499f851c91618e29f9b030d93
rod <rod.casio@outlook.com>
2025-07-05 00:04:29 -0600

    Created a new file to demonstrate using how to access properties for values

---
+++ introduction/propertiesEx.js
@@ -0,0 +1,9 @@
+// Example of using a property called length
+// which outputs the value of the number of characters of a string
+
+// important to use the '.' dot operator to access properties.
+
+// Example
+console.log("My name is Rodrigo, and I'm looking forward to become a great developer".length); // output 71
+console.log("hello".length); // outputs 5
+
