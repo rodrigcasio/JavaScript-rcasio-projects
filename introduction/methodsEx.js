ad850a7265da5a5735c5feeaf0559108b24a2d43
rod <rod.casio@outlook.com>
2025-07-05 00:17:49 -0600

    Created a new file to demonstrate how to use methods for a string value. Examples included

---
+++ introduction/methodsEx.js
@@ -0,0 +1,11 @@
+// Example of using methods
+// method .toUpperCase() : converts every character of the string to uppercased
+// .trim() : removes spaces from before the string and after string
+
+console.log("Rodrigo Casio".toUpperCase()); // outputs RODRIGO CASIO
+console.log("   I dont like white spaces    ".trim()); // outputs 'I dont like white spaces'
+
+console.log("Tripiante".startsWith('T')); // outputs: true, since it does start the string with the character 'T'
+console .log("Casionator".startsWith('T'));  // output: false, since it does not start the string with the character 'T'
+
+
