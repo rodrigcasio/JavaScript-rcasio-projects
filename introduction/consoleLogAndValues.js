6f568377d3f7fa0126deb53d5963dc05ff186007
rod <rod.casio@outlook.com>
2025-07-04 23:29:42 -0600

    Created a new file for first steps into the langauge

---
+++ introduction/consoleLogAndValues.js
@@ -0,0 +1,14 @@
+// here is an example of using the console.log()
+// to run a javaScript file, we need to use 'node file.js'
+
+// here im only printing values for prcatice
+console.log("Hello everyone");
+console.log(999430);
+console.log(false);
+
+// exercise 
+console.log('JavaScript');
+console.log(2025);
+console.log(30.234);
+console.log("I'm liking this laguage");
+console.log("Today is July 4th 2025");
