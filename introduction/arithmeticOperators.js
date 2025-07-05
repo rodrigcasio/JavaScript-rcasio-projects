bc7626927d4a9dccedf8fc8e43d72c9b6e285cbc
rod <rod.casio@outlook.com>
2025-07-04 23:38:50 -0600

    Created a new file as example of using arithmetic operators

---
+++ introduction/arithmeticOperators.js
@@ -0,0 +1,10 @@
+// Examples of basic arithmetic operators and outputting them
+console.log("Results of basic arithemic operations:");
+console.log(25 + 3.5);
+console.log(2025 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
+console.log("Example of using the modulo operator:");
+console.log(42 % 3); // outputs 0 
+console.log(53 % 5); // outputs 3
