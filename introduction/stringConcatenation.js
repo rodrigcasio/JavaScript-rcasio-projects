55d97aa476305bdade5aa7bc5a864ff62f05146f
rod <rod.casio@outlook.com>
2025-07-04 23:53:09 -0600

    File created to demonstrate how to concatenate strings in JavaScript

---
+++ introduction/stringConcatenation.js
@@ -0,0 +1,7 @@
+// Example of concatenating strings
+// using a '+' operator:
+
+console.log("I like to " + "code in JavaScript");
+console.log("Rodrigo " + "Casio " + " Garcia");
+console.log("This is my " + "name.");
+
