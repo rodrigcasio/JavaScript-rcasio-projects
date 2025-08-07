a792671b321d8dbec6534744370b57f0b34f89d7
rod <rod.casio@outlook.com>
2025-08-07 16:14:21 -0600

    Add file with example to understand constructors, prototypes, and instance vs shared methods in JavaScript OOP

--- courseraPracticeIBM/customObjects.js
+++ courseraPracticeIBM/customObjects.js
@@ -5,11 +5,43 @@ function Car(make, model, year){
     this.make = make;
     this.model = model;
     this.year = year;
-    this.getname = function (){
+    this.getname = function (){            // not recommended since every instance of this constructor creates a brand new copy of this getName function()
         return make + " " + model + " " + year;
     }
 }
 
-// object 
-var object1 = new Car("Ford", "Mustang", "2023");
+// object... instances of the cosntructor Car  
+var object1 = new Car("Ford", "Mustang", "2023");   // it has a brand new getName()
 console.log(object1.getname());
+var object2 = new Car("Susuky", "lala", 2020);  // it has a brand new getName() as well.. not recommended to have functions inside the constructor
+console.log(object2.getname());
+
+// ============================
+
+// example of a prototype
+function motorcycle(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+// instances of motorcycle:
+var bike1 = new motorcycle("Honda", "H-23", 2023);
+var bike2 = new motorcycle("Italica", "f-11", 2025);
+
+console.log(bike1.model);   // outputs  H-23.
+console.log(bike2.year); // outputs 2025
+
+motorcycle.prototype.color = "red"; // adding a property to every instance of motorycycle
+
+console.log(bike1.color); // output red;
+
+bike2.color = "blue";
+console.log(bike2.color);  
+
+// adding a method to the prototype motorcycle  after declaring the cosntructor.
+motorcycle.prototype.getName = function() {
+    return this.make + " " + this.model + " " + this.year + " " + this.color;
+}
+
+console.log(bike1.getName());
+console.log(bike2.getName());
