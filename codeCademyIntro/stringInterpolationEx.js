886e7c6d33bc885f22823ee8630b78e8acc61cc7
Rodrigo Casio <rod.casio@outlook.com>
2025-08-15 15:22:32 -0600

    Changed file name

---
+++ codeCademyIntro/stringInterpolationEx.js
@@ -0,0 +1,10 @@
+// example of String interpolation
+
+// to use a template literal it is needed backticks `` and to use the placeholder of the variable
+// it is a ${variableName} inside the `` backticks
+
+myName = "Rodrigo";
+myCity = "Boston";
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
