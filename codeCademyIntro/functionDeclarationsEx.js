bc561c6bbee6b3f216f52b1ad2f4d35acaff0034
rodrigcasio <rod.casio@outlook.com>
2025-08-25 23:15:46 -0600

    Added a new file for introduction of functions' ' '

---
+++ codeCademyIntro/functionDeclarationsEx.js
@@ -0,0 +1,12 @@
+// this is an introduction to functions in JavaScript
+
+function getReminder(){
+    console.log(`Water the plants Rod!.`);
+}
+
+function greetInSpanish(){
+    console.log(`Buenas Tardes.`);
+}
+
+getReminder();
+greetInSpanish();
