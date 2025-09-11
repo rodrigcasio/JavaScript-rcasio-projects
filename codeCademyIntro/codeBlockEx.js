13841ebd814c23540fa10e743ff2c1c53a6ea70a
rodrigcasio <rod.casio@outlook.com>
2025-09-11 14:21:52 -0600

    Added an example of error shown if we log to the console a local variable outside the function

--- codeCademyIntro/codeBlockEx.js
+++ codeCademyIntro/codeBlockEx.js
@@ -3,4 +3,8 @@
 // 1
 const logVisibleLightWaves = () => {
     let lightWaves = 'Moonlight';           // 2
+    console.log(lightWaves);
 }
+
+logVisibleLightWaves();
+logVisibleLightWaves(lightWaves); // output : *reference error*
