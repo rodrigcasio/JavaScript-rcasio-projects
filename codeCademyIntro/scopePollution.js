5834f19ac6d8163caba46163a71d69cc9b951dbd
rodrigcasio <rod.casio@outlook.com>
2025-09-11 14:42:17 -0600

    Added an example of changing a variable from a code block

--- codeCademyIntro/scopePollution.js
+++ codeCademyIntro/scopePollution.js
@@ -3,9 +3,10 @@ const satellite = 'The Moon';
 const galaxy = 'The Milky Way';
 let stars = 'North Star';
 
-const callmyNightSky = () => {
-    
+const callMyNightSky = () => {   
+    stars = 'Sirius';        // change the variable inside the block code
     return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 };
 
 console.log(callMyNightSky());
+console.log(stars);     
