452abf8ea3615c30a1128f37271c8a6a2a70e46c
rodrigcasio <rod.casio@outlook.com>
2025-09-11 14:10:37 -0600

    Outputin in the console, from a function, the global variables

--- codeCademyIntro/globalScope.js
+++ codeCademyIntro/globalScope.js
@@ -1,10 +1,14 @@
 // Example of indentifying global scope
 
+// 1
 const satellite = 'The Moon';
 const galaxy = 'The Milky Way';
 const stars = 'North Star';
 
-
+// 2
 const callMyNightSky = () => {
-    return 'Night Sky: ' + satellite + ', ' + stars + ', and' + galaxy; 
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy; 
 }
+
+// 3
+console.log(callMyNightSky());
