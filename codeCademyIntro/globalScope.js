525bf387bbebf34ec4b70a57bc3f7ca478658aa6
rodrigcasio <rod.casio@outlook.com>
2025-09-11 14:06:40 -0600

    created exercise to identify global scope

---
+++ codeCademyIntro/globalScope.js
@@ -0,0 +1,10 @@
+// Example of indentifying global scope
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+
+const callMyNightSky = () => {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and' + galaxy; 
+}
