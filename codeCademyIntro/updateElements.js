6fd807bbedf8c38dd4edc81b90f7e8a82c7eef75
rodrigcasio <rod.casio@outlook.com>
2025-09-11 19:37:04 -0600

    Created "updateElements.js" to showcase how to change the elements of the array using its index number

---
+++ codeCademyIntro/updateElements.js
@@ -0,0 +1,8 @@
+// Exercise on how to update elements of an array
+
+let groceryList= ['bread', 'tomatoes', 'mil'];
+
+// changing the second element of the array
+groceryList[1] = 'avocados';
+
+console.log(groceryList);    
