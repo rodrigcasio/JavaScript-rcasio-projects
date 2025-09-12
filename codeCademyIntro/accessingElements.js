6e6b83c7c1b8321b147461641b7f66f4ce9e6020
rodrigcasio <rod.casio@outlook.com>
2025-09-11 19:32:23 -0600

    Created a file to demonstrate how to access elements from an array

---
+++ codeCademyIntro/accessingElements.js
@@ -0,0 +1,14 @@
+// Exercise to access elements of an array
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];  
+
+const listItem = famousSayings[0];      //  1. placing the first element of the array inside a varible
+console.log(listItem);      // 1.2 
+
+console.log(famousSayings[2]);  // 2. log into the console the third element of the array
+
+console.log(famousSayings[3]); // 3. trying to access an index that is beyond the last it   // output : undefined 
+
+
+
+
