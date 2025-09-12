c165261d6f688554506314d90378d09a8e0ecd43
rodrigcasio <rod.casio@outlook.com>
2025-09-11 19:52:58 -0600

    Created "theLengthProperty.js" file to demonstrate the use of the .length property

---
+++ codeCademyIntro/theLengthProperty.js
@@ -0,0 +1,6 @@
+// Exercise to understand .length property 
+
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+// 1. finding the length of the objectives array and logging into the console
+console.log(objectives.length);        // output: 3
