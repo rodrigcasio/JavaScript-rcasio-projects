e452b75724bfe5d66debd9debef29cb9e0a9e2d6
rodrigcasio <rod.casio@outlook.com>
2025-09-13 21:33:41 -0600

    Added typescript to fix nvim error pop up

--- codeCademyIntro/theWhileLoopEx.js
+++ codeCademyIntro/theWhileLoopEx.js
@@ -1,10 +1,13 @@
 // This is an example of a while loop in JavaScript
 
-
 const cards = ['diamond', 'spade', 'heart', 'club'];
 
 // Write your code below
 
-let currentCards;
+let currentCard;
+
+while(currentCard !== 'spade'){
+
+}
 
 
