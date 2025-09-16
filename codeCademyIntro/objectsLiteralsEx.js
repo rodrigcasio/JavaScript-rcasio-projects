5184b7e72f81e6ebc5d3bf5db2e56d7466b7d044
rodrigcasio <rod.casio@outlook.com>
2025-09-15 21:09:43 -0600

    Created "objectsLiterals.js" to demonstrate initialized simple objects in JS

---
+++ codeCademyIntro/objectsLiteralsEx.js
@@ -0,0 +1,14 @@
+// Here is an example of creating an object literal 'fasterShip'
+
+
+// 1.
+const fasterShip = {
+    'Fuel Type': 'Turbo Fuel',
+    color: 'silver'
+};
+
+// 2.
+const spaceship = {
+    'Fuel Type': 'diesel',
+    color: 'silver'
+};
