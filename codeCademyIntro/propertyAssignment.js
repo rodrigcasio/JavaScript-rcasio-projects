31b5027737af4e41fc223c26cc1ee61ef1b0fd13
rodrigcasio <rod.casio@outlook.com>
2025-09-19 01:05:45 -0600

    Added example and making use of functions that can help us access ey values

--- codeCademyIntro/propertyAssignment.js
+++ codeCademyIntro/propertyAssignment.js
@@ -1,5 +1,6 @@
 // Adding new properties or changing their key's values
 
+
 let spaceship = {
   'Fuel Type' : 'Turbo Fuel',
   homePlanet : 'Earth',
@@ -8,4 +9,22 @@ let spaceship = {
 };
 
 // Write your code below
+// changing the value of key color property
+// Adding a property to the object
+// Deleting a property      (deleted = undefined)
+
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 7;
+
+const valueProp = (objectName, propName) => objectName[propName];
+const colorValue = valueProp(spaceship, 'color');
+const numEngineValue = valueProp(spaceship, 'numEngines');
+
+const f = valueProp;
+const homePlanetVal = f(spaceship, 'homePlanet');
+
+delete spaceship["Secret Mission"];
+
+console.log(`${colorValue} - ${numEngineValue} - ${homePlanetVal} - ${f(spaceship, 'Secret Mission')}`);
+
 
