c71e0b5e01c7164048fd89ed77cdf8712e66be2d
rodrigcasio <rod.casio@outlook.com>
2025-11-03 17:51:52 -0600

    Import functions with destructuring from the encryptors.js module, and added functionality to use the 3 encryptor functions to encode the message

--- codeCademyIntro/IntermJs/Modules/MessageMixer/super-encoder.js
+++ codeCademyIntro/IntermJs/Modules/MessageMixer/super-encoder.js
@@ -1,9 +1,7 @@
-// Import the encryptors functions here.
-
+const { caesarCipher, symbolCipher, reverseCipher } = require(`./encryptors.js`);
 
 const encodeMessage = (str) => {
-  // Use the encryptor functions here.
-  
+  return reverseCipher(symbolCipher(caesarCipher(str, 6)));
 }
 
 const decodeMessage = (str) => {
